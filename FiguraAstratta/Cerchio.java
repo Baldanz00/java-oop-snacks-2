3b00861dbc0e70a806689b3898f8c6dee5aea18d
Baldanz00 <gioia.travaglini18@gmail.com>
2025-07-23 12:02:34 +0200

    edit

--- FiguraAstratta/Cerchio.java
+++ FiguraAstratta/Cerchio.java
@@ -1,4 +1,5 @@
 public class Cerchio extends Figura {
+    public static final double PI_GRECO = 3.14;
     private double raggio;
     public Cerchio(double raggio) {
         this.raggio = raggio;
--- FiguraAstratta/Figura.java
+++ FiguraAstratta/Figura.java
@@ -1,4 +1,4 @@
 public abstract class Figura {
-    public static final double PI_GRECO = 3.14;
+   
     public abstract double calcolaArea();
 }
